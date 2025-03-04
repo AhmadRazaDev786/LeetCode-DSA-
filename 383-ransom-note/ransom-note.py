26eeed1230401dc0e226a3a57b5ee9a491a7a0ec
Ahmad Raza <ahmadrazaofficial33@gmail.com>
2025-03-04 07:46:55 +0500

    Time: 15 ms (76.53%) | Memory: 18.1 MB (34.75%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,12 +1,20 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        disc = defaultdict(int)
-        for i in magazine:
-            disc[i] +=1
+        # disc = defaultdict(int)
+        # for i in magazine:
+        #     disc[i] +=1
         
-        for j in ransomNote:
-            if disc[j] >0:
-                disc[j]-=1
-            else:
+        # for j in ransomNote:
+        #     if disc[j] >0:
+        #         disc[j]-=1
+        #     else:
+        #         return False
+        # return True
+
+        count_ransomNote = Counter(ransomNote)
+        count_magzine = Counter(magazine)
+
+        for char, count in count_ransomNote.items():
+            if count_magzine[char] < count:
                 return False
         return True
