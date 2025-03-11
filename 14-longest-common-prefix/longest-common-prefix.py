16a90e703c44470bd50c52e00411fca3e0b2b26a
Ahmad Raza <ahmadrazaofficial33@gmail.com>
2025-03-11 12:24:46 +0500

    Time: 3 ms (28.28%) | Memory: 17.8 MB (48.70%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,5 +1,7 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
         word = strs[0]
         for i in range(1, len(strs)):
             # for j in range(len(strs[i])):
@@ -7,5 +9,7 @@ class Solution:
             while j < len(word) and j < len(strs[i]) and word[j] == strs[i][j]:
                 j += 1
             word = word[:j]
+            if word == "":
+                return ""
         
         return word
