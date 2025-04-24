1bfe1bc0811558c98d88e31b2f1197e9bf825043
Ahmad Raza <ahmadrazaofficial33@gmail.com>
2025-04-24 23:01:40 +0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (78.91%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.py
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.py
@@ -1,14 +1,11 @@
 class Solution:
     def checkOnesSegment(self, s: str) -> bool:
-        counter = True
         boolie = True
         for i in range(len(s)):
             if s[i] == '1' and not boolie:
                 return False
                 break
-            if s[i] == '1':
-                counter = False
-            elif s[i] == '0':
+            if s[i] == '0':
                 boolie = False
             
         return True
