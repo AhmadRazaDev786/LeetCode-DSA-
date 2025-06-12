14c3d03dc27be5ff4929dae3c140bf911d29401a
Ahmad Raza <ahmadrazaofficial33@gmail.com>
2025-06-12 17:50:07 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.07%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -4,11 +4,14 @@ class Solution:
             return 1
         if n == 2:
             return 2
-        a, b = 1, 2
-        c = 0
-        for i in range(3, n+1):
+        c= 0
+        a = 1
+        b = 1
+        for i in range(n-2, -1, -1):
             c = a + b
             a = b
             b = c
-        return b
+
+        return c
+
         
